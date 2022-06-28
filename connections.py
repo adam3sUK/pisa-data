4a0df4831804d723ba514e6febdba90460565492
Adam Harbisher <aharbisher@gmail.com>
2022-06-28 17:47:08 +0100

    updating count with 3 DB

--- connections.py
+++ connections.py
@@ -1,8 +1,18 @@
 import psycopg2
 
-connection = psycopg2.connect(host='seta-gbr.cvcpj1fhj3k9.us-east-2.rds.amazonaws.com', database='gbr', user='seta', password='defaultUnsafePassword')
+databases = {
+  "fra":"seta-fra.cvcpj1fhj3k9.us-east-2.rds.amazonaws.com",
+  "gbr":"seta-gbr.cvcpj1fhj3k9.us-east-2.rds.amazonaws.com",
+  "ita":"seta-ita.cvcpj1fhj3k9.us-east-2.rds.amazonaws.com"
+}
 
-cursor = connection.cursor()
-cursor.execute("SELECT COUNT(id) FROM public.responses")
 
-count = sum(cursor.fetchone())
+count = 0
+
+for k, v in databases.items():
+  connection = psycopg2.connect(host=v, database=k, user='seta', password='defaultUnsafePassword')
+  cursor = connection.cursor()
+  cursor.execute("SELECT COUNT(id) FROM responses")
+  count += sum(cursor.fetchone())
+
+print(count)
