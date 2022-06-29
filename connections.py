d296fb7228103bd455aec013163940d1854968ea
Adam Harbisher <aharbisher@gmail.com>
2022-06-29 14:55:34 +0100

    added back db connection

--- connections.py
+++ connections.py
@@ -5,17 +5,18 @@ import psycopg2
 
 class counter():
   def __init__(self, redis):
-      # self.database = database_objects
+      self.database = database_objects
       self.redis = redis
       self.redis.set("count", 0)
 
   def update_count(self):
-    self.redis.set("count", int(self.redis.get("count")) + 1)
-    # for k, v in self.database.items():
-    #   connection = psycopg2.connect(host=v, database=k, user='seta', password='defaultUnsafePassword')
-    #   cursor = connection.cursor()
-    #   cursor.execute("SELECT COUNT(id) FROM responses")
-    #   self.count += sum(cursor.fetchone())
+    count = 0
+    for k, v in self.database.items():
+      connection = psycopg2.connect(host=v, database=k, user='seta', password='defaultUnsafePassword')
+      cursor = connection.cursor()
+      cursor.execute("SELECT COUNT(id) FROM responses")
+      count += sum(cursor.fetchone())
+    self.redis.set("count", count)
 
   def get_count(self):
     return int(self.redis.get("count"))
