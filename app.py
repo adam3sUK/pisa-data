6d92e1b4d756da14fc428ac9d87404ddeeabcba3
Adam Harbisher <aharbisher@gmail.com>
2022-06-30 18:00:49 +0100

    sliming down project

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
-import connections
-import os
-import redis
 import psycopg2
-r = redis.from_url(os.environ.get("REDIS_URL"), decode_responses=True)
 
 app = Flask(__name__)
 CORS(app)
@@ -26,61 +22,61 @@ def num_submissions():
 
 
 
-@app.route("/submissions-time")
-def time_submissions():
-  return {
-  "datasets": [
-    {
-      "id": "Submissions",
-      "data": [
-        { "x": "12:00", "y": 82 },
-        { "x": "13:00", "y": 88 },
-        { "x": "14:00", "y": 101 },
-        { "x": "15:00", "y": 97 },
-        { "x": "16:00", "y": 121 },
-        { "x": "17:00", "y": 83 },
-        { "x": "18:00", "y": 59 }
-      ]
-    }
-  ]
-}
+# @app.route("/submissions-time")
+# def time_submissions():
+#   return {
+#   "datasets": [
+#     {
+#       "id": "Submissions",
+#       "data": [
+#         { "x": "12:00", "y": 82 },
+#         { "x": "13:00", "y": 88 },
+#         { "x": "14:00", "y": 101 },
+#         { "x": "15:00", "y": 97 },
+#         { "x": "16:00", "y": 121 },
+#         { "x": "17:00", "y": 83 },
+#         { "x": "18:00", "y": 59 }
+#       ]
+#     }
+#   ]
+# }
 
-@app.route("/early-education-belonging")
-def sense_belonging():
-  return {
-    "datasets": [
-      {
-        "id": "GBR",
-        "data": [
-          {
-            "x": 6,
-            "y": 1.1,
-            "submissions": 412
-          }
-        ]
-      },
-      {
-        "id": "FRA",
-        "data": [
-          {
-            "x": 5,
-            "y": 0.9,
-            "submissions": 469
-          }
-        ]
-      },
-      {
-        "id": "ESP",
-        "data": [
-          {
-            "x": 6,
-            "y": 0.4,
-            "submissions": 500
-          }
-        ]
-      }
-    ]
-  }
+# @app.route("/early-education-belonging")
+# def sense_belonging():
+#   return {
+#     "datasets": [
+#       {
+#         "id": "GBR",
+#         "data": [
+#           {
+#             "x": 6,
+#             "y": 1.1,
+#             "submissions": 412
+#           }
+#         ]
+#       },
+#       {
+#         "id": "FRA",
+#         "data": [
+#           {
+#             "x": 5,
+#             "y": 0.9,
+#             "submissions": 469
+#           }
+#         ]
+#       },
+#       {
+#         "id": "ESP",
+#         "data": [
+#           {
+#             "x": 6,
+#             "y": 0.4,
+#             "submissions": 500
+#           }
+#         ]
+#       }
+#     ]
+#   }
 
 if __name__ == '__main__':
   app.run()
